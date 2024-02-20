d16889c22ff3fb4c10373d0233ed6fdef5298f63
georgesyrigos <georgesirigosaek@gmail.com>
2024-02-20 12:08:13 +0200

    changes and signup activity

--- app/src/main/java/com/example/android_teliki_v2/MainActivity.java
+++ app/src/main/java/com/example/android_teliki_v2/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView4 = findViewById(R.id.textView4);
+        textView4.setText("Notification for the nearest dangers\n\nFloods, Fires, Earthquakes, Other natural disasters");
 
     }
     public void login(View view){
@@ -19,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
     }
 
-    public class hellogeorge{
-
+    public void signup(View view){
+        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(intent);
     }
 
     public class world{
