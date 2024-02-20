19c033071621602655efe7b7558aa935f4cfea44
georgesyrigos <georgesirigosaek@gmail.com>
2024-02-20 11:35:39 +0200

    welcome page and login activity

--- app/src/main/java/com/example/android_teliki_v2/MainActivity.java
+++ app/src/main/java/com/example/android_teliki_v2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android_teliki_v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-    public class hello {
-
+    public void login(View view){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
     }
 
     public class hellogeorge{
