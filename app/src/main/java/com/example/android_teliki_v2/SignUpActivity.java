55f6d918be445b2da01fa510c4e6d428ee57eb5b
georgesyrigos <georgesirigosaek@gmail.com>
2024-02-22 16:36:17 +0200

    sign up user to firestore

--- app/src/main/java/com/example/android_teliki_v2/SignUpActivity.java
+++ app/src/main/java/com/example/android_teliki_v2/SignUpActivity.java
@@ -1,12 +1,25 @@
 package com.example.android_teliki_v2;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SignUpActivity extends AppCompatActivity {
@@ -14,6 +27,7 @@ public class SignUpActivity extends AppCompatActivity {
     Button mSignupBtn;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
+    String userID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +40,37 @@ public class SignUpActivity extends AppCompatActivity {
         mSignupBtn = findViewById(R.id.signupButton);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+
+
+    }
+    public void signup(View view){
+        String username = mUsername.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        String email = mEmail.getText().toString().trim();
+        String role = mRole.getText().toString().trim();
+
+        if(TextUtils.isEmpty(username)) {
+            mUsername.setError("Username is Required!");
+        }
+        fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener((task) -> {
+            if (task.isSuccessful()){
+                Toast.makeText(SignUpActivity.this, "User Created", Toast.LENGTH_SHORT).show();
+                userID = fAuth.getCurrentUser().getUid();
+                DocumentReference documentReference = fStore.collection("users").document(userID);
+                Map<String,Object> user = new HashMap<>();
+                user.put("username",username);
+                user.put("email",email);
+                user.put("role",role);
+                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                    }
+                });
+            }
+        });
+
     }
 }
