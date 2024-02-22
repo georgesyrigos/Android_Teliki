f08c8ffc7f31470e214256ae05a673891c9c4d9a
georgesyrigos <georgesirigosaek@gmail.com>
2024-02-22 14:00:33 +0200

    signup layout

--- app/src/main/java/com/example/android_teliki_v2/SignUpActivity.java
+++ app/src/main/java/com/example/android_teliki_v2/SignUpActivity.java
@@ -3,12 +3,28 @@ package com.example.android_teliki_v2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 
 public class SignUpActivity extends AppCompatActivity {
-
+    EditText mUsername,mPassword,mEmail,mRole;
+    Button mSignupBtn;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mUsername = findViewById(R.id.UsernameText);
+        mPassword = findViewById(R.id.PasswordText);
+        mEmail = findViewById(R.id.EmailAddressText);
+        mRole = findViewById(R.id.RoleText);
+        mSignupBtn = findViewById(R.id.signupButton);
+
+        fAuth = FirebaseAuth.getInstance();
     }
 }
