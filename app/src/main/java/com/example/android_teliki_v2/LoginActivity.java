6296ff7fb400d95b1d895db45b6902343dfba664
georgesyrigos <georgesirigosaek@gmail.com>
2024-02-23 12:34:18 +0200

    login user to firebase

--- app/src/main/java/com/example/android_teliki_v2/LoginActivity.java
+++ app/src/main/java/com/example/android_teliki_v2/LoginActivity.java
@@ -1,14 +1,65 @@
 package com.example.android_teliki_v2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText mPassword,mEmail;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.EmailLoginText);
+        mPassword = findViewById(R.id.PasswordLoginText);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
     }
+
+    public void login(View view) {
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)) {
+            mEmail.setError("Email is Required!");
+            return;
+        }
+        if(TextUtils.isEmpty(password)) {
+            mPassword.setError("Password is Required!");
+            return;
+        }
+
+        //authenticate the user
+        fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+    }
+
 }
